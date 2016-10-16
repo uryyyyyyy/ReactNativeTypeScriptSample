b291a7845cf6c0cf30811f5d051811ecbf2e95c4
shibatako <ko.shibata@opt.ne.jp>
2016-10-16 14:59:14 +0900

    typescript

---
+++ src/HelloWorld.tsx
@@ -0,0 +1,12 @@
+/// <reference path="../typings/index.d.ts" />
+
+import * as React from 'react';
+import {Text} from 'react-native';
+
+export default class HelloWorld extends React.Component<{}, {}> {
+    render() {
+        return (
+            <Text>へろぅわああああーるど</Text>
+        );
+    }
+}
