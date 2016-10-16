870363813453288241e19555edab835935e8c0f3
shibatako <ko.shibata@opt.ne.jp>
2016-10-16 16:16:05 +0900

    react-native-kit

--- src/home/Counter.tsx
+++ src/home/Counter.tsx
@@ -3,6 +3,13 @@ import {GlobalState} from "./Entities";
 import {DispatchActions} from "./DispatchActions";
 import {Text, TouchableHighlight, View} from 'react-native';
 
+const MK = require('react-native-material-kit');
+
+const {
+    MKButton,
+    MKColor,
+} = MK;
+
 interface Props {
     value: GlobalState;
     actions: DispatchActions;
@@ -11,18 +18,22 @@ interface Props {
 export class Counter extends React.Component<Props, {}> {
 
     render() {
+        const ColoredRaisedButton = MKButton.coloredButton()
+            .withText('increment 3')
+            .withOnPress(() => this.props.actions.increment(3))
+            .build();
+
+        const ColoredRaisedButton2 = MKButton.coloredButton()
+            .withText('decrement 2')
+            .withOnPress(() => this.props.actions.decrement(2))
+            .build();
+
         return (
             <View style={{padding: 30}}>
                 <Text>へろぅわーるど</Text>
                 <Text>Score: {this.props.value.num}</Text>
-
-                <TouchableHighlight onPress={() => this.props.actions.increment(3)}>
-                    <Text>increment 3</Text>
-                </TouchableHighlight>
-
-                <TouchableHighlight onPress={() => this.props.actions.decrement(2)}>
-                    <Text>decrement 2</Text>
-                </TouchableHighlight>
+                <ColoredRaisedButton/>
+                <ColoredRaisedButton2/>
             </View>
         )
     }
